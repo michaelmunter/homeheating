a66a1a6ec0fb4a9bc83c8b723a5b05424da5488c
michaelmunter <iammunter@gmail.com>
2023-05-22 11:12:57 +0200

    added calc components-fix2

--- src/pages/calculator/Actions.tsx
+++ src/pages/calculator/Actions.tsx
@@ -1,10 +1,4 @@
-export default function Actions({
-  color,
-  handleClick,
-}: {
-  color: string
-  handleClick: () => void
-}) {
+export default function Actions({ handleClick }: { handleClick: () => void }) {
   return (
     <button
       className="text-l mt-4 w-fit rounded-md bg-red-900 p-3  text-white"
