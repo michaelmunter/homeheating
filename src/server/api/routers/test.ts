fd13f0ff015fdba1c4c35e053b3c0ebc8e74879e
michaelmunter <iammunter@gmail.com>
2023-05-22 11:16:28 +0200

    added calc components-fix4

--- src/server/api/routers/test.ts
+++ src/server/api/routers/test.ts
@@ -1,6 +1,6 @@
 import { z } from "zod"
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
-import { TRPCError } from "@trpc/server"
+
 import testCalc from "~/server/calculations/testCalc"
 
 export const testRouter = createTRPCRouter({
@@ -13,8 +13,7 @@ export const testRouter = createTRPCRouter({
   mutateAdder: publicProcedure
     .input(z.object({ n1: z.number(), n2: z.number() }))
 
-    .mutation(async ({ input }) => {
-      //const result = input.n1 + input.n2
+    .mutation(({ input }) => {
       const result = testCalc(input.n1, input.n2)
 
       return result
