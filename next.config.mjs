2b1c426756636dc7c26cbda729c3032b1714bb84
michaelmunter <iammunter@gmail.com>
2023-06-02 17:11:50 +0200

    swcMinify:false

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import("./src/env.mjs")
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: false,
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
@@ -18,5 +19,5 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
-export default config;
+}
+export default config
