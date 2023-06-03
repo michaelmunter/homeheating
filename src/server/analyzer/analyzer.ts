a3db7c694bb446f8eaa849fcb1be36468b14d044
michaelmunter <iammunter@gmail.com>
2023-06-03 22:23:41 +0200

    test

--- src/server/analyzer/analyzer.ts
+++ src/server/analyzer/analyzer.ts
@@ -1,4 +1,4 @@
-// import { type Climate } from "@prisma/client"
+import { type Climate } from "@prisma/client"
 
 // export type BaseData = {
 //   heatLossFactor: number
@@ -38,19 +38,19 @@
 //   }[]
 // }
 
-// type UserSpecs = {
-//   heatLossFactor: number
-//   buildYear: number
-//   area: number
-//   heatDist: string
-//   residents: number
-//   tempSetting: number
-//   location: string
-//   systems: {
-//     type: string
-//     COP: number
-//   }[]
-// }
+type UserSpecs = {
+  heatLossFactor: number
+  buildYear: number
+  area: number
+  heatDist: string
+  residents: number
+  tempSetting: number
+  location: string
+  systems: {
+    type: string
+    COP: number
+  }[]
+}
 
 // export type Base = {
 //   Tout_limit: number
@@ -62,37 +62,37 @@
 //   d: { Qroom: number; Two: number }[]
 // }
 
-// // interface awPump extends SystemData {}
-// // interface aaPump extends SystemData {}
-// // interface gwPump extends SystemData {}
-// // type SystemsType = (awPump | aaPump | gwPump)[]
+// interface awPump extends SystemData {}
+// interface aaPump extends SystemData {}
+// interface gwPump extends SystemData {}
+// type SystemsType = (awPump | aaPump | gwPump)[]
 
-// export default function analyzer(u: UserSpecs, climate: Climate[]) {
-//   //CALCULATE BASE DATA
-//   const b: Base = {
-//     Tout_limit: u.tempSetting > 18 ? u.tempSetting - 4 : u.tempSetting - 1,
-//     Two_17c: u.heatDist === "radiators" ? 45 : 30,
-//     Two_neg12c: u.heatDist === "radiators" ? 55 : 35,
-//     Twvb: 50,
-//     QroomDim:
-//       ((26 -
-//         0.442 * (u.buildYear - 2000) +
-//         0.0139 * (u.buildYear - 2000) ** 2 -
-//         0.000113 * (u.buildYear - 2000) ** 3) *
-//         u.area) /
-//       1000, // !!! antager helårshus
-//     Qwvb: u.residents ? (u.residents * 800 + 800) / (365 * 24) : 0, // !!! burde det ikke være minus 800?
-//     d: [{ Qroom: 0, Two: 0 }],
-//   }
-//   const preCalc = (b.Two_neg12c - b.Two_17c) / 29
+export default function analyzer(u: UserSpecs, climate: Climate[]) {
+  //CALCULATE BASE DATA
+  // const b: Base = {
+  //   Tout_limit: u.tempSetting > 18 ? u.tempSetting - 4 : u.tempSetting - 1,
+  //   Two_17c: u.heatDist === "radiators" ? 45 : 30,
+  //   Two_neg12c: u.heatDist === "radiators" ? 55 : 35,
+  //   Twvb: 50,
+  //   QroomDim:
+  //     ((26 -
+  //       0.442 * (u.buildYear - 2000) +
+  //       0.0139 * (u.buildYear - 2000) ** 2 -
+  //       0.000113 * (u.buildYear - 2000) ** 3) *
+  //       u.area) /
+  //     1000, // !!! antager helårshus
+  //   Qwvb: u.residents ? (u.residents * 800 + 800) / (365 * 24) : 0, // !!! burde det ikke være minus 800?
+  //   d: [{ Qroom: 0, Two: 0 }],
+  // }
+  // const preCalc = (b.Two_neg12c - b.Two_17c) / 29
 
-//   climate.forEach((row) => {
-//     b.d.push({
-//       Qroom: (b.QroomDim / 32) * Math.max(b.Tout_limit - row.air_temp, 0),
-//       Two: b.Two_17c + preCalc * (17 - row.air_temp),
-//     })
-//   })
-//   b.d.shift() //remove dummy element required for initialization = {Qroom: 0, Two: 0}
-
-//   return { b, climate }
-// }
+  // climate.forEach((row) => {
+  //   b.d.push({
+  //     Qroom: (b.QroomDim / 32) * Math.max(b.Tout_limit - row.air_temp, 0),
+  //     Two: b.Two_17c + preCalc * (17 - row.air_temp),
+  //   })
+  // })
+  // b.d.shift() //remove dummy element required for initialization = {Qroom: 0, Two: 0}
+  return "hello"
+  // return { b, climate }
+}
